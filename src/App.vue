95dacfeed53de0c099fe5996f124b551abeb5668
Death Stroke <jocileudosousav@gmail.com>
2020-03-13 17:33:29 -0300

    Insert background, duck icon, growing animation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <img class="icon" :class="crescer ? 'crescer_pato' : '' " @click.prevent="playSound" src="/svg/duck.svg" alt="">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data(){
+    return {
+      crescer: false,
+    }
+  },
+  methods:{
+     playSound () {
+      this.crescer = true;
+        var audio = new Audio(`/sound/Duck-quack.mp3`);
+        audio.play();
+
+        audio.addEventListener('ended', () => {
+          this.crescer = false;
+        })
+    }
   }
 }
 </script>
 
 <style>
+
+.crescer_pato{
+  animation: crescer 0.5s;
+}
+
+@keyframes crescer{
+  0% {
+    width: 35%;
+  }
+  25% {
+    width: 33%;
+  }
+  75% {
+    width: 44%;
+  }
+  100% {
+    width: 35%;
+  }
+}
+
 #app {
+  display: inline-flex;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 450px;
+  width: 100%;
+  background: url('/media/background.jpg');
+}
+
+.icon{
+    width: 35%;
+    display: flex;
+    margin: auto auto auto;
 }
 </style>
